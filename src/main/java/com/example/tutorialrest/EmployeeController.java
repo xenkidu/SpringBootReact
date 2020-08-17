50224cb69257df9f52100bd4a68897a84aa80b6c
xenkidu <aburton85@live.com>
2020-08-17 15:50:33 -0500

    API now includes hypermedia.

--- src/main/java/com/example/tutorialrest/EmployeeController.java
+++ src/main/java/com/example/tutorialrest/EmployeeController.java
@@ -1,7 +1,12 @@
 package com.example.tutorialrest;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.*;
+
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,13 +20,22 @@ public class EmployeeController {
     
     private final EmployeeRepository repository;
 
-    EmployeeController(EmployeeRepository repository) {
+    private final EmployeeModelAssembler assembler;
+
+    EmployeeController(EmployeeRepository repository, EmployeeModelAssembler assembler) {
         this.repository = repository;
+        this.assembler = assembler;
     }
 
     @GetMapping("/employees")
-    List<Employee> all() {
-        return repository.findAll();
+    CollectionModel<EntityModel<Employee>> all() {
+        
+        List<EntityModel<Employee>> employees = repository.findAll().stream()
+            .map(assembler::toModel)
+                .collect(Collectors.toList());
+
+        return CollectionModel.of(employees,
+            linkTo(methodOn(EmployeeController.class).all()).withSelfRel());
     }
 
     @PostMapping("/employees")
@@ -30,8 +44,12 @@ public class EmployeeController {
     }
 
     @GetMapping("/employees/{id}")
-    Employee one(@PathVariable Long id) {
-        return repository.findById(id).orElseThrow(() -> new EmployeeNotFoundException(id));
+    EntityModel<Employee> one(@PathVariable Long id) {
+    
+      Employee employee = repository.findById(id) //
+          .orElseThrow(() -> new EmployeeNotFoundException(id));
+    
+      return assembler.toModel(employee);
     }
 
     @PutMapping("/employees/{id}")
